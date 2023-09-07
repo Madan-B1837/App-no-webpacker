0786d706d3b4704cf0b315cdc836eafb6c0f0c5c
Madan Bharadwaj <madanb@bitlasoft.com>
2023-09-07 10:58:26 +0530

    Stock controller User controller and my portfolio addition

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,10 @@
 class Stock < ApplicationRecord
     def self.new_lookup(ticker_Symbol)
         client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:api_key],secret_token: 'sk_548ad2bee28e43288a11266bce9426eb',endpoint: 'https://cloud.iexapis.com/v1')
-        client.price(ticker_Symbol)
+    begin
+        new(ticker: ticker_Symbol, name: client.company(ticker_Symbol).company_name, last_price: client.price(ticker_Symbol))
+    rescue => exception
+        return nil
     end
+end
 end
